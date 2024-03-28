7458598509f3233ae92e2465dbc108d94c2344e2
kartik <kartik@convenevc.com>
2024-03-28 12:22:25 +0530

    initial RNapp

---
+++ src/components/HelloScreen.js
@@ -0,0 +1,27 @@
+// HelloScreen.js
+
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const HelloScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default HelloScreen;
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,32 @@
+// HomeScreen.js
+
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to Hello Screen"
+        onPress={() => navigation.navigate('Hello')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+export default HomeScreen;
